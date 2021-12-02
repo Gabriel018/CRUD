3df7c60db4b9f6539466a7ce1a5d9128ccf983df
Gabriel018 <duke.comandos@gmail.com>
2021-12-02 01:42:31 -0300

    1.1

--- formulario.py
+++ formulario.py
@@ -10,31 +10,38 @@ class Self:
 
 class Formulario():
 
-    def __init__(self):
-
-        self.layout = [[py.Text("Nome"), py.Input(key='name')],
-                       [py.Text("Sobrenome"), py.Input(key='name_sobre')],
-                       [py.Text("Email"), py.Input(key='Email')],
-                       [py.Button("Cadastrar", key='add')]
-                       ]
+    def __init__(self,):
+        clientes = database.view_all(self)
+        self.layout = [ [ py.Text("Cadastro de Clientes",font="arianl 16",text_color='black',justification = 'center')],
+                       [py.Text("Nome:",font="arial 14",text_color="black",pad=5),py.Input(size=(40,1),key='name'),],
+                       [py.Text("Sobrenome:",font="arial 14",text_color="black",pad=5),py.Input(size=(40,2),key='name_sobre')],
+                       [py.Text("Email:",font="arial 14",text_color="black"), py.Input(size=(40,2),key='Email')],
+                       [py.Button("Cadastrar", key='add',font="arial 12"),
+                        py.Button("Alterar", key='alter',font="arial 12"),
+                        py.Button("Deletar", key='del',font="arial 12")],
+                        [py.Listbox(values=[clientes],size=(50,10),key='Box')  ]]
 
     def Init(self):
 
-        self.janela = py.Window("Fomurlario de clientes", self.layout, size=(400, 300))
+        self.janela = py.Window("Fomurlario de clientes", self.layout, size=(600, 500))
 
         while True:
 
             self.event, self.values = self.janela.read()
+            if self.event == py.WIN_CLOSED or self.event == 'Cancel':  # if user closes window or clicks cancel
+                break
 
             if self.event == "add":
                 Nome = self.values['name']
                 Sobre_Nome = self.values['name_sobre']
                 Email = self.values['Email']
-
+                self.clientes_view = database.view_all(self)
                 database.add_clientes(self,Nome, Sobre_Nome, Email)
+
                 self.janela.find_element('name').update('')
                 self.janela.find_element('name_sobre').update('')
                 self.janela.find_element('Email').update('')
+                self.janela.find_element('Box').update(self.clientes_view)
 
                 py.popup("Cadastrado com sucesso")
 
